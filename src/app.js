22208d42237d9026ec4ba126198fc80714c8c83b
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-11-24 20:58:17 +0600

    hook route added

--- src/app.js
+++ src/app.js
@@ -6,4 +6,9 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
+app.post("/hook", (req, res) => {
+    res.status(200).json({message: "Hi!"})
+    return
+})
+
 app.listen(port, () => {console.log(`App listening on port: ${port}`)})
