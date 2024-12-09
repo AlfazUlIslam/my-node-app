67e9ba3bcfbe4f506fcf89fec88a1bd594ea6040
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-09 19:11:28 +0600

    Webhook controller modification 3

--- src/controllers/testControllers/webhook.js
+++ src/controllers/testControllers/webhook.js
@@ -1,9 +1,8 @@
 const webhook = (req, res) => {
-    let str = ""
     const bodyData = req.body
 
-    srt += "o"
-    res.status(200).send(str)
+    console.log(bodyData)
+    res.status(200).send("Webhook data recieved")
     return
 }
 
